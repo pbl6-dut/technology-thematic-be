f3e2cf77e997543f78fa9c839f2bf9c8b70d923b
Lê Huỳnh Nhật Long <72820955+Long-le-mt@users.noreply.github.com>
2022-12-07 21:54:38 +0700

    Integrate sentry (#14)
    Co-authored-by: Long Nhật <longle.24hdev@gmail.com>

--- src/bin/www.js
+++ src/bin/www.js
@@ -1,10 +1,8 @@
 import app from 'index';
 import logger from 'configs/winston.config';
 // eslint-disable-next-line import/extensions
-import registerWithEureka from 'technology_thematic_lib/helpers/eurekaHelper.js';
 
 app.set('port', process.env.PORT || 3000);
-registerWithEureka('node-service', process.env.PORT);
 
 app.listen(app.get('port'), () => {
   logger.info(`Express server listening on port ${app.get('port')}`);
