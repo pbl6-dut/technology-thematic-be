61a967f0399c0a75655f1168b36fa4bbe985b174
Doan Tan Ty <31429574+doantanty92@users.noreply.github.com>
2022-12-07 21:10:13 +0700

    Add log in auth controller (#13)
    Co-authored-by: Doan Tan Ty <tydt@nal.vn>

--- src/routers/auth.routes.js
+++ src/routers/auth.routes.js
@@ -5,49 +5,6 @@ import AuthMiddleware from 'middlewares/auth';
 
 const router = express.Router();
 
-/**
- * @swagger
- * tags:
- *   name: Auth
- *   description: The authentication managing API
- */
-
-/**
- * @swagger
- * /register:
- *   post:
- *     summary: Create a new user
- *     tags: [Auth]
- *     parameters:
- *      - in: body
- *        name: user
- *        description: The user to create.
- *        schema:
- *          type: object
- *          required:
- *            - name
- *            - email
- *            - password
- *          properties:
- *            name:
- *              type: string
- *              example: "doantanty92"
- *              description: The name of the user.
- *            email:
- *              type: string
- *              example: "doantanty92@gmail.com"
- *              description: The email of the user.
- *            password:
- *              type: string
- *              example: "123456"
- *              description: The password of the user.
- *     responses:
- *      200:
- *          description: The user was successfully created
- *      500:
- *          description: Some server error
- */
-
 router.post('/register', ValidatorBody('register'), authController.register);
 
 router.post('/login', ValidatorBody('login'), authController.login);
