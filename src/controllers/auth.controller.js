b995afa70035967f039bdb001222231841848c76
Doan Tan Ty <tydt@nal.vn>
2022-12-07 21:07:41 +0700

    Add log in auth controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import { AuthService } from 'services';
 import Response from 'helpers/response';
+import logger from 'configs/winston.config';
 
 class AuthController {
   constructor(service) {
@@ -18,8 +19,10 @@ class AuthController {
   async register(req, res) {
     try {
       const user = await this.service.signUp(req.body);
+      logger.info(`User ${user.email} has been created`);
       return Response.success(res, { docs: user }, 201);
     } catch (error) {
+      logger.error('Error in register', error, req.body);
       return Response.error(res, error);
     }
   }
@@ -27,8 +30,10 @@ class AuthController {
   async login(req, res) {
     try {
       const docs = await this.service.signIn(req.body);
+      logger.info(`User ${req.body.email} has been logged in`);
       return Response.success(res, { docs });
     } catch (error) {
+      logger.error('Error in login', error, req.body);
       return Response.error(res, error);
     }
   }
@@ -37,8 +42,10 @@ class AuthController {
     try {
       const { idOAuth } = req.jwt;
       const docs = await this.service.logout(idOAuth);
+      logger.info(`User with idOauth ${idOAuth} has been logged out`);
       return Response.success(res, { docs });
     } catch (error) {
+      logger.error('Error in logout', error, req.body);
       return Response.error(res, error);
     }
   }
@@ -46,8 +53,10 @@ class AuthController {
   async confirmEmail(req, res) {
     try {
       const docs = await this.service.confirmEmail(req.params.confirmToken);
+      logger.info(`Confirm email with token ${req.params.confirmToken}`);
       return Response.success(res, { docs });
     } catch (error) {
+      logger.error('Error in confirm email', error, req.body);
       return Response.error(res, error);
     }
   }
@@ -55,8 +64,10 @@ class AuthController {
   async refreshToken(req, res) {
     try {
       const docs = await this.service.refreshToken(req.body.refreshToken);
+      logger.info(`Refresh token with req ${req.body}`);
       return Response.success(res, { docs });
     } catch (error) {
+      logger.error('Error in refresh token', error, req.body);
       return Response.error(res, error);
     }
   }
@@ -65,8 +76,10 @@ class AuthController {
     try {
       const { idOAuth } = req.jwt;
       const docs = await this.service.getMe(idOAuth);
+      logger.info(`User ${docs.email} has been get me`);
       return Response.success(res, { docs });
     } catch (error) {
+      logger.error('Error in get me', error, req.body);
       return Response.error(res, error);
     }
   }
@@ -75,8 +88,10 @@ class AuthController {
     try {
       const { email } = req.body;
       const docs = await this.service.forgotPassword(email);
+      logger.info(`Forgot password with req ${req.body}`);
       return Response.success(res, { docs });
     } catch (error) {
+      logger.error('Error in forgot password', error, req.body);
       return Response.error(res, error);
     }
   }
@@ -85,8 +100,10 @@ class AuthController {
     try {
       const { email, verifyCode } = req.body;
       const docs = await this.service.verifyCode({ email, verifyCode });
+      logger.info(`Verify code with req ${req.body}`);
       return Response.success(res, { docs });
     } catch (error) {
+      logger.error('Error in verify code', error, req.body);
       return Response.error(res, error);
     }
   }
@@ -99,8 +116,10 @@ class AuthController {
         verifyCode,
         password,
       });
+      logger.info(`User ${email} has been reset password`);
       return Response.success(res, { docs });
     } catch (error) {
+      logger.error('Error in reset password', error, req.body);
       return Response.error(res, error);
     }
   }
