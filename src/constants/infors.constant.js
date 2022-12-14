316a94235f7c411cd8d389493351ec9584b2d1c4
Lê Huỳnh Nhật Long <72820955+Long-le-mt@users.noreply.github.com>
2022-12-14 21:36:59 +0700

    Format log in auth controller (#15)
    Co-authored-by: Long Nhật <longle.24hdev@gmail.com>

--- src/constants/infors.constant.js
+++ src/constants/infors.constant.js
@@ -1,20 +1,20 @@
 export default {
-  CREATE_AT_REPO_SUCCESS: 'Create model {0} successfully',
-  FIND_BY_ID_AT_REPO_SUCCESS: 'Find by id model {0} successfully',
-  FIND_AT_REPO_SUCCESS: 'Find model {0} successfully',
+  CREATE_AT_REPO_SUCCESS: '[Repo] Create model {0} successfully',
+  FIND_BY_ID_AT_REPO_SUCCESS: '[Repo] Find by id model {0} successfully',
+  FIND_AT_REPO_SUCCESS: '[Repo] Find model {0} successfully',
   FIND_ONE_BY_CONDITION_AT_REPO_SUCCESS:
-    'Find one model by condition {0} successfully',
+    '[Repo] Find one model by condition {0} successfully',
   FIND_ALL_BY_CONDITION_AT_REPO_SUCCESS:
-    'Find all model by condition {0} successfully',
+    '[Repo] Find all model by condition {0} successfully',
   UPDATE_BY_CONDITION_AT_REPO_SUCCESS:
-    'update model {0} by condition successfully',
-  DELETE_AT_REPO_SUCCESS: 'delete model {0} successfully',
-  UPLOAD_FILE_TO_CLOUD_SUCCESS: 'upload file to cloudinary successfully',
+    '[Repo] update model {0} by condition successfully',
+  DELETE_AT_REPO_SUCCESS: '[Repo] Delete model {0} successfully',
+  UPLOAD_FILE_TO_CLOUD_SUCCESS: '[Repo] Upload file to cloudinary successfully',
   DELETE_BY_CONDITION_AT_REPO_SUCCESS:
-    'Delete model {0} by condition successfully',
+    '[Repo] Delete model {0} by condition successfully',
 
   // Middleware
-  AUTHORIZATION_SUCCESS: '{0}: authorization successfully',
+  AUTHORIZATION_SUCCESS: '[Repo] {0}: authorization successfully',
 
   // AuthService
   LOGIN_SUCCESS: 'Login successfully',
