6491cb279f094bba2b2a1a62e7e88bc79e68c65e
Doan Tan Ty <doantanty92@gmail.com>
2022-12-14 21:56:51 +0700

    Integrate technology thematic lib

--- src/services/sections.service.js
+++ src/services/sections.service.js
@@ -1,5 +1,5 @@
 import { SectionsRepository } from 'repositories';
-import { json } from 'utils';
+import { json } from 'technology_thematic_lib/utils';
 import BaseService from './base.service';
 
 class SectionsService extends BaseService {
